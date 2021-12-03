8b2b182290fe814bd2b33679880a2153415be844
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-03 00:38:32 +0000

    Fetch data about flyover times wth callback waterfall

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -32,3 +32,21 @@ fetchISSFlyOverTimes({ latitude: 43.6694, longitude: -79.4018 }, (error, flyOver
   }
   console.log('It worked! Returned flyover times:' , flyOverTimes);
 });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
+
+const printPassTimes = passTimes => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
